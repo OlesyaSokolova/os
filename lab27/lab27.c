0f95a0399d5cf694d075065ddc2f3200a42ef8d2
Olesya <45124328+OlesyaSokolova@users.noreply.github.com>
2020-06-05 15:48:12 +0700

    Update lab27.c

--- lab27/lab27.c
+++ lab27/lab27.c
@@ -4,34 +4,50 @@
 
 int main(int argc, char ** argv)
 {
+	if (argc < 2)
+	{
+		printf("Usage: %s file_name\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
 
-    if(argc < 2)
-    {
-        printf("Usage: %s file_name\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
+	char command[BUFSIZ] = "cat ";
+	strcat(command, argv[1]);
+	strcat(command, " | grep '^$' | wc -l\0");
 
-    char command[BUFSIZ] = "cat ";
-    strcat(command, argv[1]);
-    strcat(command, " | grep '^$' | wc -l\0");
+	FILE * fin;
+	fin = popen(command, "r");
 
-    FILE * fin;
-    fin = popen(command, "r");
-    if(fin == NULL)
-    {
-	    perror(argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	if (fin == NULL)
+	{
+		perror(argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    int linesNumber;
-    fscanf(fin, "%d", &linesNumber);
-    printf("Lines number: %d\n", linesNumber);
+	int linesNumber;
+	fscanf(fin, "%d", &linesNumber);
 
-    int checkClose = pclose(fin);
-    if(checkClose == -1)
-    {
-        perror("pclose");
-        exit(EXIT_FAILURE);
-    }
-    exit(EXIT_SUCCESS);
+	int status = pclose(fin);
+	if (status == -1)
+	{
+		perror("pclose error");
+		exit(EXIT_FAILURE);
+	}
+	if (WIFEXITED(status) != 0)
+	{
+		if (WEXITSTATUS(status) == 0)
+		{
+			printf("Lines number: %d\n", linesNumber);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			printf("Error: child process returned %d", WEXITSTATUS(status));
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		perror("Child process finished with error");
+		exit(EXIT_FAILURE);
+	}	
 }
